71822022bc9222e508a0a97dffc8abcbbb61426a
AnkitBista <ankitbista1406@gmail.com>
2025-02-03 17:00:25 -0500

    Update .gitignore and other necessary files

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,21 @@
 import os 
-import sys
-from src.exception import CustomException
+
 
 class DataCleaner:
     def remove_json(self):
         """ This function is used to clean the json file that was inside the dataset
         """
-        try:
-            use_dir = 'dataset'
-            files = os.listdir(use_dir)
-            for file in files:
-                if file == '.DS_Store':
-                    continue
-                file_path = os.path.join(use_dir,file)
-                image_path = os.listdir(file_path)
-                for image_name in image_path:
-                    image = os.path.join(file_path, image_name)
-                    if image.endswith('json'):
-                        os.remove(image)
-        except Exception as e:
-            raise CustomException(e,sys)
+        use_dir = 'dataset'
+        files = os.listdir(use_dir)
+        for file in files:
+            if file == '.DS_Store':
+                continue
+            file_path = os.path.join(use_dir,file)
+            image_path = os.listdir(file_path)
+            for image_name in image_path:
+                image = os.path.join(file_path, image_name)
+                if image.endswith('json'):
+                    os.remove(image)
+    
 
       
